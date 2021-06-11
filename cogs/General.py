07891731e7cce52a3fe482ab972592c5623c9455
Drained1 <81043109+Drained1@users.noreply.github.com>
2021-06-11 15:12:04 -0500

    Update General.py

--- cogs/General.py
+++ cogs/General.py
@@ -10,7 +10,28 @@ class General(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
-        await ctx.send("https://discord.com/oauth2/authorize?client_id=845503955038896128&permissions=0&scope=bot\nWebsite: https://KhanBot.justbeveb.repl.co")
+        embed = discord.Embed(
+            title = "my links",
+            description = "[Invite Link](https://discord.com/oauth2/authorize?client_id=845503955038896128&permissions=0&scope=bot)\n[Website](https://KhanBot.justbeveb.repl.co)"
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(description='Shows the bots latency/response time\nUsage: ping\nAliases: None')
+    @commands.cooldown(1, 3, BucketType.user)
+    async def ping(self, ctx):
+        channel = ctx.message.channel
+        t1 = time.perf_counter()
+        await channel.trigger_typing()
+        t2 = time.perf_counter()
+        embed=discord.Embed(title=None, description='Ping: {} MS'.format(round((t2-t1)*1000)))
+        await channel.send(embed=embed)
+
+    @commands.command()
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title = "Starky Bot",
+            description = f"```Guilds: {len(client.guilds)}\nUnique Users: {len(client.users)}\nOwner: Drained#5313\n[Website](https://KhanBot.justbeveb.repl.co)```"
+        )
 
 def setup(client):
     client.add_cog(General(client))
